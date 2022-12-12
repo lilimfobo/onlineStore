eff937cc00f02c8a78cfca2aab68963f633e2068
Zintle Mfobo <102562771+lilimfobo@users.noreply.github.com>
2022-12-12 08:47:05 +0200

    Update Result.php

--- Result.php
+++ Result.php
@@ -146,7 +146,7 @@ if(!isset($_SESSION['user']))
       <div id="searchbox" class="container-fluid" style="width:112%;margin-left:-6%;margin-right:-6%;background:#002244;">
           <div>
               <form role="search" method="POST" action="Result.php">
-                  <input type="text" class="form-control" name="keyword" style="width:80%;margin:20px 10% 20px 10%;" placeholder="Search for a Funko Or Category">
+                  <input type="text" class="form-control" name="keyword" style="width:80%;margin:20px 10% 20px 10%;" placeholder="Search for a Funko Pop Or Category">
               </form>
           </div>
       </div>
