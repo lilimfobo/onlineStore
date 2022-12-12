843e92f397cbcc9c60370730e24c74a82f569b28
Zintle Mfobo <102562771+lilimfobo@users.noreply.github.com>
2022-12-12 09:20:18 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Funko Pops">
+    <meta name="author" content="Zintle Mfobo">
+    <title> About Us </title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+
+    <!-- Had to pull in individual styles for page -->
+    <style>
+        body{
+            padding-top: 10px;
+            padding-bottom: 20px;
+        }
+
+        #main > :first-child{
+	            margin-top: 20px;
+        }
+        
+        .card-img-top{
+            width: 1530px;
+            height: 500px;
+            margin-left: -78px;
+        }
+        #card-text{
+            font-size: 50px;
+        }
+        /* Media queries to make site responsive */
+  	@media(max-width:767px){
+        #query_button {padding: 5px 20px;}
+  	}
+    </style>
+</head>
+<body>
+  <!-- Navbar -->
+  <nav class="navbar navbar-default navbar-fixed-top navbar-inverse">
+      <div class="container-fluid" style="background:#002244;">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only"> Toggle navigation </span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="index.php" style="color:#fff;"> <<< Go back to Forever Books </a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav navbar-right">
+        </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav>
+
+<!-- Brief overview of company -->
+<h1 class="text-center"> Wo are we at Funko World? </h1>
+<div class="card">
+<img src="images/About/about1.webp" class="card-img-top" alt="manga poster">
+  <div class="card-body text-center">
+  <h4 class="mt-5">
+  <br> Funko World is a labour of love from two South African women of colour who love everything sci-fi and fantasy. We 
+  found a gap in the SA market for people like us who enjoy Marvel, Star Wars, Harry Potter and many other franchises, but couldn't enjoy
+  all the merch available in other countries. That's when we opened our doors in 2015 and haven't closed them since. <br>
+  </h4>
+  </div>
+</div>
+
+<!-- Our mission statement -->
+<h1 class="text-center"> Our mission? </h1>
+<div class="card mb-5">
+  <img src="images/About/about2.jpg" class="card-img-top" alt="drawing of girl child">
+  <div class="card-body mt-8">
+    <h4 class="text-center mt-8">
+        <br> 
+        Our mission is, and will continue to be, to bring joy, happiness and childlike wonder into an otherwise bleak world. <br> 
+</h4>
+  </div>
+</div>
+
+<!-- Our Staff -->
+<h1 class="text-center"> Cheer to our staff! </h1>
+<div class="card mb-5">
+  <img src="images/About/about3.jpg" class="card-img-top" alt="staff">
+  <div class="card-body mt-8">
+    <h4 class="text-center mt-8">
+        <br> 
+            These beautiful individuals are our amazing staff behind the scenes! <br>
+            They handle all our technical aspects of our business as well as manage our storefront. <br>
+  </div>
+</div>
+<!-- End of page -->
